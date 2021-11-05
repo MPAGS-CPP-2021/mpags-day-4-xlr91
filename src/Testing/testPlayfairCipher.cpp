d58aa79ba7ed37c256a1cc15e74a240b81247705
Emir <emirmuhammad08@yahoo.com>
2021-11-05 10:48:38 +0000

    Add skeleton tests for Playfair

---
+++ src/Testing/testPlayfairCipher.cpp
@@ -0,0 +1,21 @@
+//! Unit Tests for MPAGSCipher CaesarCipher Class
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "PlayfairCipher.hpp"
+
+TEST_CASE("Playfair Cipher encryption", "[playfair]")
+{
+    //CaesarCipher cc{10};
+    //REQUIRE(cc.applyCipher("HELLOWORLD", CipherMode::Encrypt) == "ROVVYGYBVN");
+
+    REQUIRE(true);
+}
+
+TEST_CASE("Playfair Cipher decryption", "[playfair]")
+{
+    //CaesarCipher cc{10};
+    //REQUIRE(cc.applyCipher("ROVVYGYBVN", CipherMode::Decrypt) == "HELLOWORLD");
+
+    REQUIRE(true);
+}
