6aa55e17d431f871e773a07c6b35273311d0ef60
Emir <emirmuhammad08@yahoo.com>
2021-11-05 10:48:24 +0000

    Implement skeleton Playfair Cipher

---
+++ src/MPAGSCipher/PlayfairCipher.cpp
@@ -0,0 +1,55 @@
+#include "PlayfairCipher.hpp"
+
+#include <string>
+
+PlayfairCipher::PlayfairCipher(const std::string& key){
+    setKey(key);
+}
+
+
+void PlayfairCipher::setKey(const std::string& key){
+    //store original key
+    key_ = key;
+
+    //append alphabet
+
+    //make sure key is upper case
+     
+    //remove non-alpha characters
+
+    //change j->i
+
+    //remove duplicate letters
+
+    //store coord of each letter
+
+    //store playfair cipher key map
+}
+
+std::string PlayfairCipher::applyCipher(const std::string& inputText, const CipherMode cipherMode)const {
+    //change j->i
+
+    //if repeated chars in a digraph add an x or q if xx
+
+    //if size of input is odd, add trailing z
+
+    //loop over inputs in digraphs
+
+    //  find the corods in grid of each digraph
+
+    // appl rules to get new corod
+
+    //find letter associated
+
+    //return text
+    
+    
+    std::string outputtext = inputText;
+    switch (cipherMode) {
+        case CipherMode::Encrypt:
+            break;
+    case CipherMode::Decrypt:
+            break;
+    }
+    return "Hello, World!";
+}
---
+++ src/MPAGSCipher/PlayfairCipher.hpp
@@ -0,0 +1,49 @@
+#ifndef MPAGSCIPHER_PLAYFAIRCIPHER_HPP
+#define MPAGSCIPHER_PLAYFAIRCIPHER_HPP
+
+#include "CipherMode.hpp"
+#include <string>
+
+
+/**
+ * \file PlayfairCipher.hpp
+ * \brief Contains the declaration of the PlayfairCipher class
+ */
+
+/**
+ * \class PlayfairCipher
+ * \brief Encrypt or decrypt text using the Playfair cipher with the given key
+ */
+
+
+
+class PlayfairCipher {
+  public:
+      /**
+     * \brief Create a new PlayfairCipher with the given key
+     *
+     * \param key the key to use in the cipher
+     */
+    explicit PlayfairCipher(const std::string& key);
+
+      /**
+     * \brief Sets the key
+     *
+     * \param key the key to use in the cipher
+     */
+    void setKey(const std::string& key);
+
+        /**
+     * \brief Apply the cipher to the provided text
+     *
+     * \param inputText the text to encrypt or decrypt
+     * \param cipherMode whether to encrypt or decrypt the input text
+     * \return the result of applying the cipher to the input text
+     */
+    std::string applyCipher(const std::string& inputText, const CipherMode cipherMode) const;
+
+  private:
+    std::string key_{0};
+};
+
+#endif
